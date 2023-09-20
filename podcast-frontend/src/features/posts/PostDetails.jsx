f2782460b525dca6f26a75a8f0006a823be75dda
Ukaypromise <promiseukaimo@gmail.com>
2023-09-20 10:48:31 +0100

    Add routing and post show page

--- podcast-frontend/src/features/posts/PostDetails.jsx
+++ podcast-frontend/src/features/posts/PostDetails.jsx
@@ -4,6 +4,7 @@ import { API_URL } from "../../constants";
 
 const PostDetails = () => {
   const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
   const { id } = useParams();
   const [post, setPost] = useState(null);
   useEffect(() => {
@@ -13,16 +14,22 @@ const PostDetails = () => {
         if (response.ok) {
           const json = await response.json();
           setPost(json);
+          setIsLoading(false);
         } else {
           throw new Error("Something went wrong");
         }
       } catch (error) {
         setError(error.toString());
         console.log(error);
+        
       }
     }
     fetchCurrentPost();
   }, [id]);
+
+  if(isLoading) {
+    return <p>Loading...</p>
+  }
   return (
     <div>
       <h2>Post Details</h2>
@@ -30,6 +37,7 @@ const PostDetails = () => {
       {post && (
         <>
           <h3>{post.title}</h3>
+            <p>{console.log(post)}</p>
           <p>{post.description}</p>
           <Link to="/">Back to all posts</Link>
         </>
--- podcast-frontend/src/routes/AppRoutes.jsx
+++ podcast-frontend/src/routes/AppRoutes.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PostsList from "../features/posts/PostsList";
-import PostDetails from "../features/posts/PostsList";
+import PostDetails from "../features/posts/PostDetails";
 
 import { Route, Routes } from "react-router-dom";
 
@@ -8,7 +8,7 @@ const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<PostsList />} />
-      <Route path="posts/:id" element={<PostDetails />} />
+      <Route path="/:id" element={<PostDetails />} />
     </Routes>
   );
 };
